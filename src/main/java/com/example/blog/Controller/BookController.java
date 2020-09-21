619c3ee06a58e10f0630c89ff69ece57fb92e87c
jharna agrawal <jharna.agrawal_cs16@gla.ac.in>
2020-09-21 15:39:55 +0530

    multiple postmapping

--- src/main/java/com/example/blog/Controller/BookController.java
+++ src/main/java/com/example/blog/Controller/BookController.java
@@ -1,5 +1,8 @@
 package com.example.blog.Controller;
 
+import java.util.*;
+
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.blog.model.Book;
 import com.example.blog.repository.BookRepository;
 
+
+
 @RestController
 public class BookController {
 	@Autowired
 	BookRepository bookRepository;
 	@PostMapping("/bookSave")
-	public Book insertBook( @Valid @RequestBody Book book) {
-		
+	public Book insertBook(@Valid @RequestBody Book book) {
 		return bookRepository.save(book); 
+	}
+	@PostMapping("/MultipleBookSave")
+	public String insertBook( @RequestBody List<Book> book) {
+		bookRepository.saveAll(book); 
+		return "record is saved successfully";
 	}
 
 }
