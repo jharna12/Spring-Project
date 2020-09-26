17d25e47f6266fe5980ac0dc661fa53828f6d421
jharna agrawal <jharna.agrawal_cs16@gla.ac.in>
2020-09-26 14:24:10 +0530

    update apppoints

--- src/main/java/com/example/blog/Controller/BookController.java
+++ src/main/java/com/example/blog/Controller/BookController.java
@@ -7,24 +7,26 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.blog.model.Book;
 import com.example.blog.repository.BookRepository;
 
 
-
-@RestController
+@RestController @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1")
 public class BookController {
 	@Autowired
 	BookRepository bookRepository;
-	@PostMapping("/bookSave")
+	@PostMapping("/book")
 	public Book insertBook(@Valid @RequestBody Book book) {
 		return bookRepository.save(book); 
 	}
@@ -33,7 +35,7 @@ public class BookController {
 		bookRepository.saveAll(book); 
 		return "record is saved successfully";
 	}
-	@GetMapping("/GetAllBook")
+	@GetMapping("/book")
 	public List<Book> getBook() {
 		return  bookRepository.findAll(); 
 	}
@@ -41,7 +43,7 @@ public class BookController {
 	public List<Book> getBook(@PathVariable("name") String bookTitle) {
 		return  bookRepository.findBybookTitle(bookTitle); 
 	}
-	@GetMapping("/GetBookById/{bookId}")
+	@GetMapping("/book/{bookId}")
 	public Optional<Book> getBook(@PathVariable("bookId") Long id) {
 		return  bookRepository.findById(id); 
 		
@@ -51,14 +53,14 @@ public class BookController {
 		return bookRepository.save(book); 
 	}
 
-	@DeleteMapping("/DeleteBookById/{bookId}")
+	@DeleteMapping("/book/{bookId}")
 	public String DeleteBook(@PathVariable("bookId") Long id) {
 		Book b=bookRepository.getOne(id);
 		 bookRepository.deleteById(id); 
 		 return "deleted";
 		
 	}
-	@PutMapping("/UpdateBook/{bookId}")
+	@PutMapping("/book/{bookId}")
 	public ResponseEntity<Book> updateBook(@PathVariable("bookId") Long id,  @RequestBody Book bookDetails)
 	{
 		Book book= bookRepository.findById(id).get();
